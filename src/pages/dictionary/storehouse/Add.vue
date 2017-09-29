6df1e2fd2343830b876fde86df1ca16ec70d08f7
梁云斌 <yliang@ivisyt.com>
2017-09-29 13:40:08 +0800

    updated route module

---
+++ src/pages/dictionary/storehouse/Add.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+
+
+      }
+    },
+
+    created(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/router/routes/personal.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Administrator on 2017/9/29 0029.
+ */
+export default [
+  {
+    path: '/personal/security/security', //账户安全
+    component: resolve => require(['@/pages/personal/security/Security'], resolve)
+  },
+  {
+    path: '/personal/company/basicinfo', //基础信息
+    component: resolve => require(['@/pages/personal/company/BasicInfo'], resolve)
+  },
+  {
+    path: '/personal/company/department', //员工部门信息
+    component: resolve => require(['@/pages/personal/company/Department'], resolve)
+  },
+  {
+    path: '/personal/client/clientmanagement', //客户管理
+    component: resolve => require(['@/pages/personal/client/ClientManagement'], resolve)
+  },
+  {
+    path: '/personal/client/inputclient', //客户导入
+    component: resolve => require(['@/pages/personal/client/InputClient'], resolve)
+  },
+  {
+    path: '/personal/client/updateclient', //修改客户详情
+    component: resolve => require(['@/pages/personal/client/UpdateClient'], resolve)
+  },
+  {
+    path: '/personal/client/createclient', //新增客户
+    component: resolve => require(['@/pages/personal/client/CreateClient'], resolve)
+  },
+
+  {
+    path: '/personal/role/list', //角色列表
+    component: resolve => require(['@/pages/personal/role/list'], resolve)
+  },
+  {
+    path: '/personal/role/add', //新建角色
+    component: resolve => require(['@/pages/personal/role/add'], resolve)
+  },
+  {
+    path: '/personal/role/update', //修改角色
+    component: resolve => require(['@/pages/personal/role/update'], resolve)
+  },
+]
